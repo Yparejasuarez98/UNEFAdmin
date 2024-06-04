4202e13bb00417dd8ea4e6dedb9f24293ae11070
Yparejasuarez98 <ypareja@ditech.es>
2024-06-04 02:04:03 -0500

    eliminar path adicional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'empresas' },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
     { path: 'login', loadChildren: () => import('./auth/login/login.routes').then(m => m.LOGIN_ROUTES) },
     { path: '', loadChildren: () => import('./components/component.routes').then(m => m.COMPONENT_ROUTES)},
 ];
--- src/app/auth/login/login.routes.ts
+++ src/app/auth/login/login.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login.component';
 
 export const LOGIN_ROUTES: Routes = [
     {
-        path: 'login',
+        path: '',
         component: LoginComponent
     }
 ]
